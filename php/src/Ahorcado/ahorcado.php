1bdc1245ae41cf80e133bc99a52e3c86b7ef31da
IvanaPlamenovaMikinska <ivana200123@gmail.com>
2024-09-24 19:44:08 +0200

    AHORCADO

---
+++ php/src/Ahorcado/ahorcado.php
@@ -0,0 +1,49 @@
+<?php
+include "funciones.php";
+
+$palabra = "enchufe";
+$arrayAdivinadas = ["_", "_", "_", "_", "_", "_", "_"];
+$mensaje = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $letra = $_POST["letra"];
+
+    if (comprobarIntentos($palabra, $letra, $arrayAdivinadas)) {
+        $mensaje = "<span class='correct'>Letra correcta</span>";
+    } else {
+        $mensaje = "<span class='incorrect'>Letra incorrecta</span>";
+    }
+}
+?>
+
+<html>
+
+<head>
+    <title>AHORCADO</title>
+    <style>
+        .correct {
+            color: green;
+        }
+
+        .incorrect {
+            color: red;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>AHORCADO</h1>
+
+    <p>Palabra:</p>
+    <p><?php mostrarArray($arrayAdivinadas); ?></p>
+
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <label for="letra">Letra:</label>
+        <input type="letra" id="letra" name="letra" maxlenght="1" required><br><br>
+        <input type="submit" value="Enviar">
+    </form>
+
+    <p><?php echo $mensaje; ?></p>
+</body>
+
+</html>
